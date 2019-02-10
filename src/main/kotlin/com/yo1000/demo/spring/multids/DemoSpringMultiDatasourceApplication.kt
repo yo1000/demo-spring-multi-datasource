9d5c437aeb7ea0e340b1016aaba80ebc0e82cc3d
yo1000 <yoichi.kikuchi.aug@gmail.com>
2019-02-10 22:18:18 +0900

    Initial commit

---
+++ src/main/kotlin/com/yo1000/demo/spring/multids/DemoSpringMultiDatasourceApplication.kt
@@ -0,0 +1,12 @@
+package com.yo1000.demo.spring.multids
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoSpringMultiDatasourceApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoSpringMultiDatasourceApplication>(*args)
+}
+
