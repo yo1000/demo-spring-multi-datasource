7ad453f572c63c28d58abb5d71b382e9b6f220ec
yo1000 <yoichi.kikuchi.aug@gmail.com>
2019-02-11 21:15:28 +0900

    Add Multi JDBC Config

--- src/main/kotlin/com/yo1000/demo/spring/multids/DemoSpringMultiDatasourceApplication.kt
+++ src/main/kotlin/com/yo1000/demo/spring/multids/DemoSpringMultiDatasourceApplication.kt
@@ -1,7 +1,17 @@
 package com.yo1000.demo.spring.multids
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.jdbc.DataSourceBuilder
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
+import javax.sql.DataSource
 
 @SpringBootApplication
 class DemoSpringMultiDatasourceApplication
@@ -10,3 +20,58 @@ fun main(args: Array<String>) {
 	runApplication<DemoSpringMultiDatasourceApplication>(*args)
 }
 
+/*
+ * Primary JDBC Config
+ */
+@Primary
+@Configuration
+@ConfigurationProperties(prefix = "spring.datasource")
+class PrimaryDataSourceProperties : DataSourceProperties()
+
+interface PrimaryNamedParameterJdbcOperations : NamedParameterJdbcOperations
+
+class PrimaryNamedParameterJdbcTemplate(dataSource: DataSource)
+	: NamedParameterJdbcTemplate(dataSource), PrimaryNamedParameterJdbcOperations
+
+/*
+ * Secondary JDBC Config
+ */
+@Configuration
+@ConfigurationProperties(prefix = "spring.datasource-secondary")
+class SecondaryDataSourceProperties : DataSourceProperties()
+
+interface SecondaryNamedParameterJdbcOperations : NamedParameterJdbcOperations
+
+class SecondaryNamedParameterJdbcTemplate(dataSource: DataSource)
+	: NamedParameterJdbcTemplate(dataSource), SecondaryNamedParameterJdbcOperations
+
+/*
+ * Bean Config
+ */
+@Configuration
+@EnableConfigurationProperties
+class JdbcConfig {
+	@Bean
+	fun primaryNamedParameterJdbcOperations(
+			props: PrimaryDataSourceProperties
+	): PrimaryNamedParameterJdbcOperations = PrimaryNamedParameterJdbcTemplate(
+			DataSourceBuilder.create()
+					.driverClassName(props.driverClassName)
+					.url(props.url)
+					.username(props.username)
+					.password(props.password)
+					.build()
+	)
+
+	@Bean
+	fun secondaryNamedParameterJdbcOperations(
+			props: SecondaryDataSourceProperties
+	): SecondaryNamedParameterJdbcOperations = SecondaryNamedParameterJdbcTemplate(
+			DataSourceBuilder.create()
+					.driverClassName(props.driverClassName)
+					.url(props.url)
+					.username(props.username)
+					.password(props.password)
+					.build()
+	)
+}
--- src/test/kotlin/com/yo1000/demo/spring/multids/DemoSpringMultiDatasourceApplicationTests.kt
+++ src/test/kotlin/com/yo1000/demo/spring/multids/DemoSpringMultiDatasourceApplicationTests.kt
@@ -2,16 +2,34 @@ package com.yo1000.demo.spring.multids
 
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
 class DemoSpringMultiDatasourceApplicationTests {
+	@Autowired
+	lateinit var primaryNamedParameterJdbcOperations: PrimaryNamedParameterJdbcOperations
+	@Autowired
+	lateinit var secondaryNamedParameterJdbcOperations: SecondaryNamedParameterJdbcOperations
 
 	@Test
 	fun contextLoads() {
+		primaryNamedParameterJdbcOperations.execute("""
+    	CREATE TABLE demo_primaries(
+    		id		INT(10)			NOT NULL,
+    		name	VARCHAR(80)		NOT NULL,
+    		PRIMARY KEY(id)
+    	)
+		""".trimIndent()) {}
+
+		secondaryNamedParameterJdbcOperations.execute("""
+    	CREATE TABLE demo_secondaries(
+    		id		INT(10)			NOT NULL,
+    		name	VARCHAR(80)		NOT NULL,
+    		PRIMARY KEY(id)
+    	)
+		""".trimIndent()) {}
 	}
-
 }
-
